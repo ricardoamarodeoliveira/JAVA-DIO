782f38bf43a9938e665c8e9001258df8a98469ce
ricardoamarodeoliveira <ricardoamarofoto@gmail.com>
2024-05-25 18:03:51 -0300

    maio 25

---
+++ Primeiro projeto em JAVA/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,24 @@
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Ricardo";
+        String segundoNome = "Amaro de Oliveira";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    
+    }
+
+        public static String nomeCompleto (String primeiroNome, String segundoNome){
+
+            return "PACIENTE: " + primeiroNome.concat(" ").concat(segundoNome);
+        }
+
+
+
+
+
+
+
+    
+}
