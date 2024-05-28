5d73dc6addf0469cf68eccee7922c1db7e9bb2cf
ricardoamarodeoliveira <ricardoamarofoto@gmail.com>
2024-05-27 22:14:05 -0300

    27 de maio

---
+++ Primeiro projeto em JAVA/java-anatomia-classes/src/ric/amaro/avaliacaofisica/Operadores.java
@@ -0,0 +1,46 @@
+package ric.amaro.avaliacaofisica;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        int simboloIgual = 100; // simbolo que se refere a atribuição, ou seja, que recebe
+        // simbolos de + - * / representam o basico como soma por exemplo. pode usar o + para juntar duas palavras igual exemplo abaixo
+
+        String nomeCompleto = "java " + "completo";
+
+        System.out.print(nomeCompleto);
+
+        //abaixo exemplo de como podemos acrescentar um numero a uma variavel dentro de um while por exemplo, tipo linhaLida + 1 basta usar linha++. 
+        //o mesmo pode ser feito para o linhaLida-- para tirar um numero
+
+        int linhaLida = 1;
+
+        linhaLida++;
+
+        linhaLida++;
+
+        System.out.println(linhaLida);
+
+        //abaixo boleanos que por nromalmente vem como false pode ser alterado no meio do caminho com o simbolo de ! na frente da variavel, conforme abaixo 
+        //na variavel returnVariavel
+
+        boolean returnVariavel = true;
+        returnVariavel = !returnVariavel;
+        System.out.println(returnVariavel);
+
+        // para comprar podemos usar o simbolo == para representar se algo é igual a outro.
+
+        // podemos usar && para perguntar se algo E algo é verdadeiro ou podemos usar || para perguntar se algo OU algo é verdadeiro
+
+
+
+       
+        
+
+     
+    
+    }
+
+
+    
+}
---
+++ Primeiro projeto em JAVA/java-anatomia-classes/src/ric/amaro/avaliacaofisica/Variaveis.java
@@ -0,0 +1,18 @@
+package ric.amaro.avaliacaofisica;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int dataNascimento = 1984;
+        double salarioMinimo = 1400.99;
+        float pi = 3.14f; //tipo float precisa sempre encerrar com f ou F
+
+        final int SALARIO_RECEBIDO = 12300; // usa o final no inicio para dizer que esse int não pode ser alterado, usa a viarivel em caixa alta
+        
+
+     
+    
+    }
+
+
+    
+}
